c27cc45892f29d69c2807debe7cdc5b6fdcd1856
ilerik <user@LAPTOP-HJKKDMLP>
2025-01-12 15:18:15 +0300

    more works

--- app/constants.ts
+++ app/constants.ts
@@ -2,7 +2,7 @@ export const paintings = [
     { 
       id: 1, 
       title: `Schiaparelli`, 
-      src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_7_2025-01-12_11-32-29.jpg-XukiJGVYOpiQUAh0Vazm4Etzh7qp64.jpeg`,
+      src: `/works/schiaparelli-ss-2024.jpg`,
       description: `A minimalist fashion illustration capturing the essence of luxury through a white double-breasted coat. The dramatic white brushstrokes against the brown background create a striking contrast that emphasizes the garment's elegant silhouette.`,
       span: true,
       type: `physical`
@@ -10,7 +10,7 @@ export const paintings = [
     { 
       id: 2, 
       title: `Sporty Chic`, 
-      src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_6_2025-01-12_11-32-29.jpg-vhhAdJLFOyED4oEVETOpENXFqjlZnO.jpeg`,
+      src: `/works/miumiu-celeb-ss-2024.jpg`,
       description: `A contemporary portrait showcasing urban sportswear, featuring bold red tones and dynamic composition that bridges fashion and street style.`,
       type: `digital`,
       sold: true
@@ -18,35 +18,35 @@ export const paintings = [
     { 
       id: 3, 
       title: `Pop Culture`, 
-      src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_3_2025-01-12_11-32-29.jpg-0APcGnAb6dy8Nq9ttv5juUJisd0GgF.jpeg`,
+      src: `/works/enjoy.jpg`,
       description: `A playful take on pop culture iconography, featuring vibrant reds and expressive portraiture that challenges brand recognition and identity.`,
       type: `digital`
     },
     { 
       id: 4, 
       title: `Spring Bloom`, 
-      src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_4_2025-01-12_11-32-29.jpg-EHJEDuAMfkCTyuD10cE7X6ziLsI3Y9.jpeg`,
+      src: `/works/spring-bloom.jpg`,
       description: `A delicate watercolor portrait where nature meets beauty, featuring subtle floral elements and striking blue eyes that draw the viewer into a moment of contemplation.`,
       type: `physical`
     },
     { 
       id: 5, 
-      title: `Fashion Statement`, 
-      src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_2_2025-01-12_11-32-29.jpg-40nH4Z3J8Sb60EfWfFu97OfhucL701.jpeg`,
+      title: `Lockdown`, 
+      src: `/works/lockdown.jpg`,
       description: `A bold commentary on luxury fashion, featuring a designer-inspired mask rendered in rich golds and deep blacks, exploring themes of identity and branding.`,
       type: `digital`
     },
     { 
       id: 6, 
       title: `Golden Feathers`, 
-      src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_1_2025-01-12_11-32-29.jpg-qFrrRpmq2SJUOy00esbZp0QehGt6f2.jpeg`,
+      src: `/works/schiaparelli.jpg`,
       description: `An opulent portrait adorned with intricate golden feathers, combining fashion and natural elements in a harmonious celebration of beauty and texture.`,      
       type: `physical`
     },
     { 
         id: 7, 
-        title: `Mother Russia`, 
-        src: `/works/mother_russia.jpg`,
+        title: `Dior SS24 Collection`, 
+        src: `/works/dior-ss24-collection.jpg `,
         description: `A delicate watercolor portrait where nature meets beauty, featuring subtle floral elements and striking blue eyes that draw the viewer into a moment of contemplation.`,        
         type: `physical`
     },    
@@ -78,27 +78,40 @@ export const paintings = [
         src: `/works/dior-backstage-ss-2024.jpg`,
         description: `A delicate watercolor portrait where nature meets beauty, featuring subtle floral elements and striking blue eyes that draw the viewer into a moment of contemplation.`,        
         type: `physical`
-    }
+    },
+    { 
+        id: 11, 
+        title: `Dior SS2024`, 
+        src: `/works/dior-ss2024.jpg`,
+        description: `A delicate watercolor portrait where nature meets beauty, featuring subtle floral elements and striking blue eyes that draw the viewer into a moment of contemplation.`,        
+        type: `physical`
+    },
+    { 
+        id: 12, 
+        title: `Giambattistavalli AW2024`, 
+        src: `/works/giambattistavalli-aw24.jpg `,
+        description: `A delicate watercolor portrait where nature meets beauty, featuring subtle floral elements and striking blue eyes that draw the viewer into a moment of contemplation.`,        
+        type: `physical`
+    },
   ]
 
 
-// /works/dior-ss24-collection.jpg 
-// /works/dior-ss2024.jpg 
-// /works/enjoy.jpg 
-// /works/giambattistavalli-aw24.jpg 
+// 
+// 
+// 
 // /works/lemon.jpg 
-// /works/lockdown.jpg 
+// 
 // /works/loewe-backstage-ss24.jpg 
 // /works/loewe-ss24.jpg 
 // /works/louis-vuitton-ss24.jpg 
-// /works/miu miu-celeb-ss-2024.jpg 
+// 
 // /works/miumiu-celeb-ss24-collection.jpg 
-// /works/mother_russia.jpg 
+//  
 // /works/russian-style.jpg 
 // /works/schiaparelli-haute-couture-2024.jpg 
 // /works/schiaparelli-haute-couture-collection24.jpg 
-// /works/schiaparelli-ss-2024.jpg /works/schiaparelli.jpg 
-// /works/spring-bloom.jpg 
+// /works/schiaparelli-ss-2024.jpg 
+//  
 // /works/streetstyle-pfw2024.jpg 
 // /works/Valentino-aw 2024.jpg 
 // /works/valentino-aw- 2024.jpg 
@@ -108,50 +121,3 @@ export const paintings = [
 // /works/ysl-ss24-part2.jpg 
 // /works/ysl-ss204.jpg 
 // /works/zendaya-metgala2024.jpg
-//   const paintings = [
-//     { 
-//       id: 1, 
-//       title: `Schiaparelli`, 
-//       src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_7_2025-01-12_11-32-29.jpg-XukiJGVYOpiQUAh0Vazm4Etzh7qp64.jpeg`,
-//       description: `A minimalist fashion illustration capturing the essence of luxury through a white double-breasted coat. The dramatic white brushstrokes against the brown background create a striking contrast that emphasizes the garment's elegant silhouette.`,
-//       span: true,
-//       type: `physical`,
-//     },
-//     { 
-//       id: 2, 
-//       title: `Sporty Chic`, 
-//       src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_6_2025-01-12_11-32-29.jpg-vhhAdJLFOyED4oEVETOpENXFqjlZnO.jpeg`,
-//       description: `A contemporary portrait showcasing urban sportswear, featuring bold red tones and dynamic composition that bridges fashion and street style.`,
-//       type: `digital`,
-//       sold: true
-//     },
-//     { 
-//       id: 3, 
-//       title: `Pop Culture`, 
-//       src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_3_2025-01-12_11-32-29.jpg-0APcGnAb6dy8Nq9ttv5juUJisd0GgF.jpeg`,
-//       description: `A playful take on pop culture iconography, featuring vibrant reds and expressive portraiture that challenges brand recognition and identity.`,
-//       type: `digital`
-//     },
-//     { 
-//       id: 4, 
-//       title: `Spring Bloom`, 
-//       src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_4_2025-01-12_11-32-29.jpg-EHJEDuAMfkCTyuD10cE7X6ziLsI3Y9.jpeg`,
-//       description: `A delicate watercolor portrait where nature meets beauty, featuring subtle floral elements and striking blue eyes that draw the viewer into a moment of contemplation.`,
-//       type: `physical`
-//     },
-//     { 
-//       id: 5, 
-//       title: `Fashion Statement`, 
-//       src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_2_2025-01-12_11-32-29.jpg-40nH4Z3J8Sb60EfWfFu97OfhucL701.jpeg`,
-//       description: `A bold commentary on luxury fashion, featuring a designer-inspired mask rendered in rich golds and deep blacks, exploring themes of identity and branding.`,
-//       type: `digital`
-//     },
-//     { 
-//       id: 6, 
-//       title: `Golden Feathers`, 
-//       src: `https://hebbkx1anhila5yf.public.blob.vercel-storage.com/photo_1_2025-01-12_11-32-29.jpg-qFrrRpmq2SJUOy00esbZp0QehGt6f2.jpeg`,
-//       description: `An opulent portrait adorned with intricate golden feathers, combining fashion and natural elements in a harmonious celebration of beauty and texture.`,
-//       span: true,
-//       type: `physical`
-//     },
-//   ]
