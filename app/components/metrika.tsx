90c575b7eda887de7f5d708399e3fa6170857c93
ilerik <user@LAPTOP-HJKKDMLP>
2025-01-14 19:35:51 +0300

    metrika

--- app/components/metrika.tsx
+++ app/components/metrika.tsx
@@ -14,7 +14,8 @@ export function Metrika() {
 
   return (
     <Script id="yandex-metrika" defer>
-      {`<!-- Yandex.Metrika counter -->        
+      {`<!-- Yandex.Metrika counter -->
+        <script type="text/javascript" >
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -27,7 +28,9 @@ export function Metrika() {
             trackLinks:true,
             accurateTrackBounce:true,
             webvisor:true
-        });                
+        });
+        </script>
+        <noscript><div><img src="https://mc.yandex.ru/watch/99524718" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
         <!-- /Yandex.Metrika counter -->
       `}
     </Script>
--- app/layout.tsx
+++ app/layout.tsx
@@ -32,26 +32,23 @@ export default function RootLayout({
         </main>
         <Footer />
         <Script id="yandex-metrika" defer>
-      {`<!-- Yandex.Metrika counter -->
-        <script type="text/javascript" >
-        (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-        (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          {`<!-- Yandex.Metrika counter -->        
+            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+            m[i].l=1*new Date();
+            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
 
-        ym(99524718, "init", {
-            defer: true,
-            clickmap:true,
-            trackLinks:true,
-            accurateTrackBounce:true,
-            webvisor:true
-        });
-        </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/99524718" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-        <!-- /Yandex.Metrika counter -->
-      `}
-    </Script>
+            ym(99524718, "init", {
+                defer: true,
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+            });                
+            <!-- /Yandex.Metrika counter -->
+          `}
+        </Script>
       </body>
     </html>
   )
