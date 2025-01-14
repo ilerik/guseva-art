5266755720204c35fc9f2e246d9adfbb2aaccf30
ilerik <user@LAPTOP-HJKKDMLP>
2025-01-14 19:31:09 +0300

    metrika

--- app/components/metrika.tsx
+++ app/components/metrika.tsx
@@ -14,8 +14,7 @@ export function Metrika() {
 
   return (
     <Script id="yandex-metrika" defer>
-      {`<!-- Yandex.Metrika counter -->
-        <script type="text/javascript" >
+      {`<!-- Yandex.Metrika counter -->        
         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
         m[i].l=1*new Date();
         for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
@@ -28,9 +27,7 @@ export function Metrika() {
             trackLinks:true,
             accurateTrackBounce:true,
             webvisor:true
-        });
-        </script>
-        <noscript><div><img src="https://mc.yandex.ru/watch/99524718" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        });                
         <!-- /Yandex.Metrika counter -->
       `}
     </Script>
