3a5f3bdb9285eca0c78780167896c96dd025aee9
ilerik <user@LAPTOP-HJKKDMLP>
2025-01-26 14:44:32 +0300

    upd translation

--- app/constants.ts
+++ app/constants.ts
@@ -12,185 +12,185 @@ export const paintings: Painting[] = [
 { 
     title: `Schiaparelli`, 
     src: `/works/schiaparelli-ss-2024.jpg`,
-    description: `A minimalist fashion illustration capturing the essence of luxury through a white double-breasted coat. The dramatic white brushstrokes against the brown background create a striking contrast that emphasizes the garment's elegant silhouette.`,
+    description: `Минималистичная модная иллюстрация, передающая суть роскоши через белое двубортное пальто. Драматичные белые мазки на коричневом фоне создают яркий контраст, подчеркивающий элегантный силуэт одежды.`,
     span: true,
-    type: `physical`
+    type: 'physical'
 },
 { 
     title: `Sporty Chic`, 
     src: `/works/miumiu-celeb-ss-2024.jpg`,
-    description: `A contemporary portrait showcasing urban sportswear, featuring bold red tones and dynamic composition that bridges fashion and street style.`,
-    type: `digital`,
+    description: `Современный портрет, демонстрирующий городскую спортивную одежду, с яркими красными тонами и динамичной композицией, объединяющей моду и уличный стиль.`,
+    type: 'digital',
     sold: true
 },
-    { 
+{ 
     title: `Pop Culture`, 
     src: `/works/enjoy.jpg`,
-    description: `A playful take on pop culture iconography, featuring vibrant reds and expressive portraiture that challenges brand recognition and identity.`,
-    type: `digital`
+    description: `Игривый взгляд на иконографию поп-культуры с яркими красными цветами и выразительным портретом, бросающим вызов узнаваемости бренда и идентичности.`,
+    type: 'digital'
 },
 { 
     title: `Spring Bloom`, 
     src: `/works/spring-bloom.jpg`,
-    description: `A delicate watercolor portrait where nature meets beauty, featuring subtle floral elements and striking blue eyes that draw the viewer into a moment of contemplation.`,
-    type: `physical`
+    description: `Нежный акварельный портрет, где природа встречается с красотой, с тонкими цветочными элементами и выразительными голубыми глазами, которые погружают зрителя в момент созерцания.`,
+    type: 'physical'
 },
 { 
     title: `Lockdown`, 
     src: `/works/lockdown.jpg`,
-    description: `A bold commentary on luxury fashion, featuring a designer-inspired mask rendered in rich golds and deep blacks, exploring themes of identity and branding.`,
-    type: `digital`
+    description: `Смелый комментарий о люксовой моде, с дизайнерской маской, выполненной в богатых золотых и глубоких черных тонах, исследующий темы идентичности и брендинга.`,
+    type: 'digital'
 },
 { 
     title: `Golden Feathers`, 
     src: `/works/schiaparelli.jpg`,
-    description: `An opulent portrait adorned with intricate golden feathers, combining fashion and natural elements in a harmonious celebration of beauty and texture.`,      
-    type: `physical`
+    description: `Роскошный портрет, украшенный изысканными золотыми перьями, сочетающий моду и природные элементы в гармоничном празднике красоты и текстуры.`,      
+    type: 'physical'
 },
 { 
     title: `Dior SS24 Collection`, 
     src: `/works/dior-ss24-collection.jpg`,
-    description: `A showcase of Dior's Spring/Summer 2024 collection, featuring elegant designs and luxurious fabrics that highlight the brand's commitment to craftsmanship and innovation.`,
-    type: `physical`
+    description: `Показ коллекции Dior Весна/Лето 2024, демонстрирующий элегантные дизайны и роскошные ткани, подчеркивающие приверженность бренда мастерству и инновациям.`,
+    type: 'physical'
 },    
 { 
     title: `Mother Russia`, 
     src: `/works/mother_russia.jpg`,
-    description: `An evocative portrait that blends traditional Russian elements with contemporary fashion, capturing the essence of cultural heritage and modern style.`,
+    description: `Эмоциональный портрет, сочетающий традиционные русские элементы с современной модой, передающий суть культурного наследия и современного стиля.`,
     span: true,
-    type: `physical`
+    type: 'physical'
 },
 { 
     title: `Balmain SS24`, 
     src: `/works/balmain-ss24.jpg`,
-    description: `A striking piece from Balmain's Spring/Summer 2024 collection, showcasing bold designs and intricate details that reflect the brand's avant-garde approach to fashion.`,
-    type: `physical`
+    description: `Яркая работа из коллекции Balmain Весна/Лето 2024, демонстрирующая смелые дизайны и сложные детали, отражающие авангардный подход бренда к моде.`,
+    type: 'physical'
 },
 { 
     title: `Dior AW24`, 
     src: `/works/dior-aw-2024-collection.jpg`,
-    description: `A highlight of Dior's Autumn/Winter 2024 collection, featuring sophisticated silhouettes and rich textures that embody the brand's timeless elegance.`,
-    type: `physical`
+    description: `Highlight коллекции Dior Осень/Зима 2024, представляющий изысканные силуэты и богатые текстуры, воплощающие вневременную элегантность бренда.`,
+    type: 'physical'
 },
 { 
     title: `Dior Backstage SS24`, 
     src: `/works/dior-backstage-ss-2024.jpg`,
-    description: `A behind-the-scenes glimpse of Dior's Spring/Summer 2024 collection, capturing the energy and creativity of the fashion show preparation.`,
-    type: `physical`
+    description: `Закулисный взгляд на коллекцию Dior Весна/Лето 2024, передающий энергию и креативность подготовки к модному показу.`,
+    type: 'physical'
 },
 { 
     title: `Dior SS2024`, 
     src: `/works/dior-ss2024.jpg`,
-    description: `A piece from Dior's Spring/Summer 2024 collection, featuring delicate floral motifs and refined craftsmanship that highlight the brand's dedication to beauty and elegance.`,
-    type: `physical`
+    description: `Работа из коллекции Dior Весна/Лето 2024, с нежными цветочными мотивами и утонченным мастерством, подчеркивающими преданность бренда красоте и элегантности.`,
+    type: 'physical'
 },
 { 
     title: `Giambattistavalli AW2024`, 
     src: `/works/giambattistavalli-aw24.jpg`,
-    description: `A showcase of Giambattistavalli's Autumn/Winter 2024 collection, featuring luxurious fabrics and intricate designs that reflect the brand's opulent style.`,
-    type: `physical`
+    description: `Показ коллекции Giambattistavalli Осень/Зима 2024, демонстрирующий роскошные ткани и сложные дизайны, отражающие роскошный стиль бренда.`,
+    type: 'physical'
 },
 // New entries
 { 
     title: `Lemon`, 
     src: `/works/lemon.jpg`,
-    description: `A vibrant depiction of a lemon, showcasing the bright yellow color and textured surface.`,
-    type: `physical`
+    description: `Яркое изображение лимона, демонстрирующее насыщенный желтый цвет и текстурированную поверхность.`,
+    type: 'physical'
 },
 { 
     title: `Loewe Backstage SS24`, 
     src: `/works/loewe-backstage-ss24.jpg`,
-    description: `A behind-the-scenes look at the Loewe SS24 collection, capturing the energy and excitement of the fashion show.`,
-    type: `physical`
+    description: `Закулисный взгляд на коллекцию Loewe SS24, передающий энергию и волнение модного показа.`,
+    type: 'physical'
 },
 { 
     title: `Loewe SS24`, 
     src: `/works/loewe-ss24.jpg`,
-    description: `A showcase of the Loewe SS24 collection, featuring bold designs and innovative fashion.`,
-    type: `physical`
+    description: `Показ коллекции Loewe SS24, представляющий смелые дизайны и инновационную моду.`,
+    type: 'physical'
 },
 { 
     title: `Louis Vuitton SS24`, 
     src: `/works/louis-vuitton-ss24.jpg`,
-    description: `A highlight of the Louis Vuitton SS24 collection, emphasizing luxury and craftsmanship.`,
-    type: `physical`
+    description: `Highlight коллекции Louis Vuitton SS24, подчеркивающий роскошь и мастерство.`,
+    type: 'physical'
 },
 { 
     title: `Miu Miu Celeb SS24 Collection`, 
     src: `/works/miumiu-celeb-ss24-collection.jpg`,
-    description: `A celebrity-inspired collection from Miu Miu, blending high fashion with star power.`,
-    type: `physical`
+    description: `Вдохновленная знаменитостями коллекция от Miu Miu, сочетающая высокую моду со звездным влиянием.`,
+    type: 'physical'
 },
 { 
     title: `Russian Style`, 
     src: `/works/russian-style.jpg`,
-    description: `An exploration of traditional Russian fashion, featuring intricate patterns and rich colors.`,
-    type: `physical`
+    description: `Исследование традиционной русской моды с замысловатыми узорами и богатыми цветами.`,
+    type: 'physical'
 },
 { 
     title: `Schiaparelli Haute Couture 2024`, 
     src: `/works/schiaparelli-haute-couture-2024.jpg`,
-    description: `A haute couture piece from Schiaparelli's 2024 collection, showcasing exquisite craftsmanship and design.`,
-    type: `physical`
+    description: `Работа от-кутюр из коллекции Schiaparelli 2024, демонстрирующая безупречное мастерство и дизайн.`,
+    type: 'physical'
 },
 { 
     title: `Schiaparelli Haute Couture Collection 24`, 
     src: `/works/schiaparelli-haute-couture-collection24.jpg`,
-    description: `A collection of haute couture pieces from Schiaparelli's 2024 line, emphasizing luxury and elegance.`,
-    type: `physical`
+    description: `Коллекция работ от-кутюр из линии Schiaparelli 2024, подчеркивающая роскошь и элегантность.`,
+    type: 'physical'
 },
 { 
     title: `Streetstyle PFW 2024`, 
     src: `/works/streetstyle-pfw2024.jpg`,
-    description: `A snapshot of street style during Paris Fashion Week 2024, capturing the latest trends and styles.`,
-    type: `physical`
+    description: `Моментальный снимок уличного стиля во время Парижской недели моды 2024, запечатлевший последние тренды и стили.`,
+    type: 'physical'
 },
 { 
     title: `Valentino AW 2024`, 
     src: `/works/Valentino-aw 2024.jpg`,
-    description: `A piece from Valentino's Autumn/Winter 2024 collection, featuring sophisticated designs and rich fabrics.`,
-    type: `physical`
+    description: `Работа из коллекции Valentino Осень/Зима 2024, представляющая изысканные дизайны и богатые ткани.`,
+    type: 'physical'
 },
 { 
     title: `Valentino AW 2024`, 
     src: `/works/valentino-aw- 2024.jpg`,
-    description: `A piece from Valentino's Autumn/Winter 2024 collection, featuring sophisticated designs and rich fabrics.`,
-    type: `physical`
+    description: `Работа из коллекции Valentino Осень/Зима 2024, представляющая изысканные дизайны и богатые ткани.`,
+    type: 'physical'
 },
 { 
     title: `Valentino AW 2024`, 
     src: `/works/valentino-aw-2024.jpg`,
-    description: `A piece from Valentino's Autumn/Winter 2024 collection, featuring sophisticated designs and rich fabrics.`,
-    type: `physical`
+    description: `Работа из коллекции Valentino Осень/Зима 2024, представляющая изысканные дизайны и богатые ткани.`,
+    type: 'physical'
 },
 { 
     title: `YSL AW 2024 Collection`, 
     src: `/works/ysl-aw-2024-collection.jpg`,
-    description: `A collection from Yves Saint Laurent's Autumn/Winter 2024 line, showcasing timeless elegance and modern style.`,
-    type: `physical`
+    description: `Коллекция Yves Saint Laurent Осень/Зима 2024, демонстрирующая вневременную элегантность и современный стиль.`,
+    type: 'physical'
 },
 { 
     title: `YSL SS24 Part 1`, 
     src: `/works/ysl-ss24-part1.jpg`,
-    description: `The first part of Yves Saint Laurent's Spring/Summer 2024 collection, featuring fresh and innovative designs.`,
-    type: `physical`
+    description: `Первая часть коллекции Yves Saint Laurent Весна/Лето 2024, представляющая свежие и инновационные дизайны.`,
+    type: 'physical'
 },
 { 
     title: `YSL SS24 Part 2`, 
     src: `/works/ysl-ss24-part2.jpg`,
-    description: `The second part of Yves Saint Laurent's Spring/Summer 2024 collection, continuing the theme of fresh and innovative designs.`,
-    type: `physical`
+    description: `Вторая часть коллекции Yves Saint Laurent Весна/Лето 2024, продолжающая тему свежих и инновационных дизайнов.`,
+    type: 'physical'
 },
 { 
     title: `YSL SS204`, 
     src: `/works/ysl-ss204.jpg`,
-    description: `A piece from Yves Saint Laurent's Spring/Summer 204 collection, showcasing timeless elegance and modern style.`,
-    type: `physical`
+    description: `Работа из коллекции Yves Saint Laurent Весна/Лето 204, демонстрирующая вневременную элегантность и современный стиль.`,
+    type: 'physical'
 },
 { 
     title: `Zendaya Met Gala 2024`, 
     src: `/works/zendaya-metgala2024.jpg`,
-    description: `A stunning portrait of Zendaya at the Met Gala 2024, capturing her elegance and style.`,
-    type: `physical`
+    description: `Потрясающий портрет Зендаи на Met Gala 2024, передающий её элегантность и стиль.`,
+    type: 'physical'
 }
 ].map((item, index) => {return { id: index, ...item }});
